55dc323be6a54916442a181a30a0731f04c786ae
Trondab <tabolset@hotmail.com>
2016-10-24 11:23:33 +0200

    undoing mistake

--- src/gittest_24_10_16/GitTest_24_10_16.java
+++ src/gittest_24_10_16/GitTest_24_10_16.java
@@ -9,8 +9,6 @@ public class GitTest_24_10_16 {
 
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("fuckalakka");
-        System.out.println("yolo");
     }
     
 }
