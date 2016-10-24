c39e98c88dbad300f8085dd320990bbd2d0657fd
Trondab <tabolset@hotmail.com>
2016-10-24 10:45:52 +0200

    commit all netbeansfile

---
+++ src/gittest_24_10_16/GitTest_24_10_16.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest_24_10_16;
+
+/**
+ *
+ * @author TrondArild
+ */
+public class GitTest_24_10_16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
