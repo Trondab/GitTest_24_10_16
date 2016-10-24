d49836742ca90cf78edd7817d5e0385c350aa0bf
Trondab <tabolset@hotmail.com>
2016-10-24 11:16:24 +0200

    random changes

--- src/gittest_24_10_16/GitTest_24_10_16.java
+++ src/gittest_24_10_16/GitTest_24_10_16.java
@@ -9,6 +9,8 @@ public class GitTest_24_10_16 {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("fuckalakka");
+        System.out.println("yolo");
     }
     
 }
